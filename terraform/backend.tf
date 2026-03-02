87adee7f29a8e2d42da165b6988faa27213acdac
ERAF-2407 <efren.arnaude@gmail.com>
2026-03-01 23:42:41 -0400

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state"
+    bucket = "efren-devsecops-tf-state-2026"
     key    = "terraform.tfstate"
-    region = "eu-west-3"
+    region = "us-east-1"
   }
 }
